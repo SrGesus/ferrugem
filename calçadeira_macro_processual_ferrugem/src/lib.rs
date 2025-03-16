bd6c308602a04d88aa62976ab62982b06e4561d3
SrGesus <108523575+SrGesus@users.noreply.github.com>
2025-03-16 02:36:33 +0000

    Tradução de rust para Português

---
+++ calçadeira_macro_processual_ferrugem/src/lib.rs
@@ -0,0 +1,125 @@
+use proc_macro::{Group, Ident, TokenStream, TokenTree};
+
+fn replace_ident(ident: Ident) -> Option<TokenTree> {
+    let ident_str = ident.to_string();
+
+    let new_str = match ident_str.as_str() {
+        "Mau" => "Err",
+        "Bom" => "Ok",
+        "Cadeia" => "String",
+        "Dicionário" => "HashMap",
+        "Predefinido" => "Default",
+        "Erro" => "Error",
+        "PodeSer" => "Option",
+        "Algum" => "Some",
+        "Nenhum" => "None",
+        "Resultado" => "Result",
+        "Próprio" => "Self",
+        "imprime" => "println",
+        "pára" => "break",
+        "assíncrona" => "async",
+        "espera" => "await",
+        "ciclo" => "loop",
+        "desloca" => "move",
+        "contentor" => "crate",
+        "código_inacessível" => "unreachable_code",
+        "como" => "as",
+        "constante" => "const",
+        "característica" => "trait",
+        "perigo" => "unsafe",
+        "em" => "in",
+        "de" => "from",
+        "dinâmico" => "dyn",
+        "desembrulha" => "unwrap",
+        "predefinido" => "default",
+        "como_ref" => "as_ref",
+        // "io" => "io",
+        "externo" => "extern",
+        "falso" => "false",
+        "função" => "fn",
+        "genial" => "super",
+        "insere" => "insert",
+        "obtem" => "get",
+        "legal" => "allow",
+        "caralho" | "pânico" | "ups" => "panic",
+        "módulo" => "mod",
+        "mutável" => "mut",
+        "novo" => "new",
+        "onde" => "where",
+        "por" => "for",
+        "para" => "for",
+        "obtem_ou_insere_com" => "get_or_insert_with",
+        "principal" => "main",
+        "público" => "pub",
+        "que" => None?,
+        "devolve" => "return",
+        "implementa" => "impl",
+        "equipara" => "match",
+        "se" => "if",
+        "ou_então" => "else",
+        "próprio" => "self",
+        "seja" => "let",
+        "estático" => "static",
+        "estrutura" => "struct",
+        "suppose" => "expect",
+        "enquanto" => "while",
+        "utiliza" => "use",
+        "torna_em" => "into",
+        "verdade" => "true",
+        "enumeração" => "enum",
+        "Grupo" => "Group",
+        "Identificador" => "Ident",
+        "FluxoDeSímbolos" => "TokenStream",
+        "ÁrvoreDeSímbolos" => "TokenTree",
+        "torna_em_cadeia" => "to_string",
+        "como_cadeia" => "as_str",
+        "extensão" => "span",
+        "Vetor" => "Vec",
+        "fluxo" => "stream",
+        "empurra" => "push",
+        "estende" => "extend",
+        "delimitador" => "delimiter",
+        "Pontuação" => "Punct",
+        "macro_processual" => "proc_macro",
+        "colecoes" => "collections",
+        _ => &ident_str,
+    };
+
+    let new_ident = Ident::new(new_str, ident.span());
+    Some(TokenTree::Ident(new_ident))
+}
+
+fn replace_tree(tok: TokenTree, out: &mut Vec<TokenTree>) {
+    match tok {
+        TokenTree::Group(group) => {
+            let mut group_elem = Vec::new();
+            replace_stream(group.stream(), &mut group_elem);
+            let mut new_stream = TokenStream::new();
+            new_stream.extend(group_elem);
+            out.push(TokenTree::Group(Group::new(group.delimiter(), new_stream)));
+        }
+        TokenTree::Ident(ident) => {
+            if let Some(ident) = replace_ident(ident) {
+                out.push(ident);
+            }
+        }
+        TokenTree::Punct(..) | TokenTree::Literal(..) => {
+            out.push(tok);
+        }
+    }
+}
+
+fn replace_stream(ts: TokenStream, out: &mut Vec<TokenTree>) {
+    for tok in ts {
+        replace_tree(tok, out)
+    }
+}
+
+#[proc_macro]
+pub fn ferrugem(item: TokenStream) -> TokenStream {
+    let mut returned = Vec::new();
+    replace_stream(item, &mut returned);
+    let mut out = TokenStream::new();
+    out.extend(returned);
+    out
+}
---
+++ ferrugem_macro_processual/src/lib.rs
@@ -0,0 +1,127 @@
+calcadeira_ferrugem::ferrugem! {
+    utiliza macro_processual::{Grupo, Identificador, FluxoDeSímbolos, ÁrvoreDeSímbolos};
+
+    função substitui_identificador(identificador: Identificador) -> PodeSer<ÁrvoreDeSímbolos> {
+        seja cadeia_identificador = identificador.torna_em_cadeia();
+
+        seja cadeia_nova = equipara cadeia_identificador.como_cadeia() {
+            "Mau" => "Err",
+            "Bom" => "Ok",
+            "Cadeia" => "String",
+            "Dicionário" => "HashMap",
+            "Predefinido" => "Default",
+            "Erro" => "Error",
+            "PodeSer" => "Option",
+            "Algum" => "Some",
+            "Nenhum" => "None",
+            "Resultado" => "Result",
+            "Próprio" => "Self",
+            "imprime" => "println",
+            "pára" => "break",
+            "assíncrona" => "async",
+            "espera" => "await",
+            "ciclo" => "loop",
+            "desloca" => "move",
+            "contentor" => "crate",
+            "código_inacessível" => "unreachable_code",
+            "como" => "as",
+            "constante" => "const",
+            "característica" => "trait",
+            "perigo" => "unsafe",
+            "em" => "in",
+            "de" => "from",
+            "dinâmico" => "dyn",
+            "desembrulha" => "unwrap",
+            "predefinido" => "default",
+            "como_ref" => "as_ref",
+            // "io" => "io",
+            "externo" => "extern",
+            "falso" => "false",
+            "função" => "fn",
+            "genial" => "super",
+            "insere" => "insert",
+            "obtem" => "get",
+            "legal" => "allow",
+            "caralho" | "pânico" | "ups" => "panic",
+            "módulo" => "mod",
+            "mutável" => "mut",
+            "novo" => "new",
+            "onde" => "where",
+            "por" => "for",
+            "para" => "for",
+            "obtem_ou_insere_com" => "get_or_insert_with",
+            "principal" => "main",
+            "público" => "pub",
+            "que" => Nenhum?,
+            "devolve" => "return",
+            "implementa" => "impl",
+            "equipara" => "match",
+            "se" => "if",
+            "ou_então" => "else",
+            "próprio" => "self",
+            "seja" => "let",
+            "estático" => "static",
+            "estrutura" => "struct",
+            "suppose" => "expect",
+            "enquanto" => "while",
+            "utiliza" => "use",
+            "torna_em" => "into",
+            "verdade" => "true",
+            "enumeração" => "enum",
+            "Grupo" => "Group",
+            "Identificador" => "Ident",
+            "FluxoDeSímbolos" => "TokenStream",
+            "ÁrvoreDeSímbolos" => "TokenTree",
+            "torna_em_cadeia" => "to_string",
+            "como_cadeia" => "as_str",
+            "extensão" => "span",
+            "Vetor" => "Vec",
+            "fluxo" => "stream",
+            "empurra" => "push",
+            "estende" => "extend",
+            "delimitador" => "delimiter",
+            "Pontuação" => "Punct",
+            "macro_processual" => "proc_macro",
+            "colecoes" => "collections",
+            _ => &cadeia_identificador,
+        };
+
+        seja identificador_novo = Identificador::novo(cadeia_nova, identificador.extensão());
+        Algum(ÁrvoreDeSímbolos::Identificador(identificador_novo))
+    }
+
+    função substitui_árvore(símbolo: ÁrvoreDeSímbolos, saída: &mutável Vetor<ÁrvoreDeSímbolos>) {
+        equipara símbolo {
+            ÁrvoreDeSímbolos::Grupo(grupo) => {
+                seja mutável elementos_grupo = Vetor::novo();
+                substitui_fluxo(grupo.fluxo(), &mutável elementos_grupo);
+                seja mutável fluxo_novo = FluxoDeSímbolos::novo();
+                fluxo_novo.estende(elementos_grupo);
+                saída.empurra(ÁrvoreDeSímbolos::Grupo(Grupo::novo(grupo.delimitador(), fluxo_novo)));
+            }
+            ÁrvoreDeSímbolos::Identificador(identifiant) => {
+                se seja Algum(identifiant) = substitui_identificador(identifiant) {
+                    saída.empurra(identifiant);
+                }
+            }
+            ÁrvoreDeSímbolos::Pontuação(..) | ÁrvoreDeSímbolos::Literal(..) => {
+                saída.empurra(símbolo);
+            }
+        }
+    }
+
+    função substitui_fluxo(fluxo_de_símbolos: FluxoDeSímbolos, saída: &mutável Vetor<ÁrvoreDeSímbolos>) {
+        por símbolo em fluxo_de_símbolos {
+            substitui_árvore(símbolo, saída)
+        }
+    }
+
+    #[macro_processual]
+    público função ferrugem(elemento: FluxoDeSímbolos) -> FluxoDeSímbolos {
+        seja mutável devolvido = Vetor::novo();
+        substitui_fluxo(elemento, &mutável devolvido);
+        seja mutável saída = FluxoDeSímbolos::novo();
+        saída.estende(devolvido);
+        saída
+    }
+}
